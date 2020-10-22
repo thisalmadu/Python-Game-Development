93a15ee28332848b4666390f2e0c196307b0576e
thisalweera <54676811+thisalweera@users.noreply.github.com>
2020-10-22 11:56:06 +0200

    UPDATE: drawing.py and SupportingTest.txt

--- drawing.py
+++ drawing.py
@@ -4,8 +4,8 @@ from pygame.locals import *
 pygame.init()
 
  # set up the window
- DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
- pygame.display.set_caption('Drawing')
+DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Drawing')
 
  # set up the colors
 BLACK = ( 0,  0,  0)
